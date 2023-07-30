9328f91f16dc0ced27b700c2458ef496ea2db8b8
BriandaNunez <140363057+BriandaNunez@users.noreply.github.com>
2023-07-29 21:36:20 -0700

    Modify the Python iris

--- iris.py
+++ iris.py
@@ -1,21 +1,17 @@
-from sklearn.linear_model import LogisticRegression
 import numpy as np
 from sklearn.datasets import load_iris
-
+from sklearn.linear_model import LogisticRegression
 
 # Load data from sklearn
 X, y = load_iris(return_X_y=True)
 
 # Train the model using regresion logistic
-clf = LogisticRegression(solver='lbfgs',
-                         max_iter=1000,
-                         multi_class='multinomial').fit(X, y)
+clf = LogisticRegression(
+    solver='lbfgs',
+    max_iter=1000,
+    multi_class='multinomial').fit(X, y)
 # Define iris types
-iris_type = {
-    0: 'setosa',
-    1: 'versicolor',
-    2: 'virginica'
-}
+iris_type = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
 
 
 # Define dummy values
@@ -28,4 +24,4 @@ X = [sepal_length, sepal_width, petal_length, petal_width]
 
 prediction = clf.predict_proba([X])
 print({'class': iris_type[np.argmax(prediction)],
-      'probability': round(max(prediction[0]), 2)})
+       'probability': round(max(prediction[0]), 2)})
